f21659a2e6195fb6a9ad06c9bbc7ec305a32d631
Ioannis Protonotarios <ioannis@protonotarios.eu>
2016-11-26 12:01:15 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generated via Twitter's Application Management:
+# https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
+bearer_token = ""
