fbb4fb8d7a0df5f9c72c306967a8efafdcac7599
Ioannis Protonotarios <ioannis@protonotarios.eu>
2016-11-27 12:49:04 +0200

    Update get-user-data.py

--- get-user-data.py
+++ get-user-data.py
@@ -4,17 +4,9 @@
 import tweepy #https://github.com/tweepy/tweepy
 import csv
 
-# ΑΡΧΙΚΕΣ ΜΕΤΑΒΛΗΤΕΣ
-
-# Εδώ βάζουμε τους λογαριασμούς Twitter που θέλουμε να κατεβάσουμε
-# Μπορούμε να βάλουμε όσους θέλουμε
-# Ενδεικτικά έχω βάλει 3 λογαριασμούς Ελλήνων πολιτικών
-# Εσείς βάλτε τους δικούς σας
-accounts = ['AdonisGeorgiadi','PanosKammenos','rachelmakri']
-
-# ΜΗΝ ΑΛΛΑΞΕΤΕ ΚΑΤΙ ΑΠΟ ΔΩ ΚΑΙ ΚΑΤΩ
-
-# Φορτώνουμε τα διαπιστευτήρια για το API του Twitter που βρίσκονται στο αρχείο config.py
+# Φόρτωση του αρχείου ρυθμίσεων config.py 
+# με τα διαπιστευτήρια για το API του Twitter 
+# και τους λογαριασμούς που θέλουμε να συλλέξουμε
 config = {}
 execfile("config.py", config)
 
@@ -26,18 +18,18 @@ api = tweepy.API(auth)
 
 
 def get_user_data(screen_name):
-	user = api.get_user(screen_name = screen_name)
+    user = api.get_user(screen_name = screen_name)
 
-	userdata = [user.id_str, user.screen_name, user.name.encode("utf-8"), user.description.encode("utf-8"), user.created_at, user.location.encode("utf-8"), user.lang, user.time_zone, user.url, user.profile_image_url, user.followers_count, user.friends_count, user.statuses_count, user.favourites_count, user.listed_count, user.verified]
+    userdata = [user.id_str, user.screen_name, user.name.encode("utf-8"), user.description.encode("utf-8"), user.created_at, user.location.encode("utf-8"), user.lang, user.time_zone, user.url, user.profile_image_url, user.followers_count, user.friends_count, user.statuses_count, user.favourites_count, user.listed_count, user.verified]
 
-	writer.writerow(userdata)
+    writer.writerow(userdata)
 	
-	pass
+    pass
 
 
 if __name__ == '__main__':
-	with open('user data.csv', 'wb') as f:
-		writer = csv.writer(f)
-		writer.writerow(['Αναγνωριστικό λογαριασμού','Όνομα λογαριασμού','Πραγματικό όνομα','Περιγραφή','Ημερομηνία δημιουργίας λογαριασμού','Τοποθεσία','Γλώσσα','Ζώνη ώρας','URL λογαριασμού','URL φωτογραφίας προφίλ','Αριθμός ακολούθων','Αριθμός ατόμων που ακολουθεί','Αριθμός tweets','Αριθμός αγαπημένων','Αριθμός λιστών στις οποίες είναι μέλος','Επαληθευμένος λογαριασμός;'])
-		for account in accounts:
-			get_user_data(account)
+    with open('user data.csv', 'wb') as f:
+        writer = csv.writer(f)
+        writer.writerow(['Αναγνωριστικό λογαριασμού','Όνομα λογαριασμού','Πραγματικό όνομα','Περιγραφή','Ημερομηνία δημιουργίας λογαριασμού','Τοποθεσία','Γλώσσα','Ζώνη ώρας','URL λογαριασμού','URL φωτογραφίας προφίλ','Αριθμός ακολούθων','Αριθμός ατόμων που ακολουθεί','Αριθμός tweets','Αριθμός αγαπημένων','Αριθμός λιστών στις οποίες είναι μέλος','Επαληθευμένος λογαριασμός;'])
+        for account in accounts:
+            get_user_data(account)
