e254b89ac375d0d0f6f25db333c59a52012c9f0e
Ioannis Protonotarios <ioannis@protonotarios.eu>
2016-11-27 12:44:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,22 @@
-#--------------------------------------------------------------------------------
-# These tokens are needed for user authentication.
-# Credentials can be generated via Twitter's Application Management:
-# https://apps.twitter.com/app/new
-#--------------------------------------------------------------------------------
+#!/usr/bin/env python
+# encoding: utf-8
+
+# ΑΡΧΕΙΟ ΡΥΘΜΙΣΕΩΝ
+
+# Καταχωρίστε τα διαπιστευτήριά σας για το API του Twitter
+# Αυτά τα διαπιστευτήρια μπορείτε να τα δημιουργήσετε
+# μέσω της Διαχείρισης Εφαρμογών του Twitter στη διεύθυνση:
+#	https://apps.twitter.com/app/new
 
 consumer_key = ""
 consumer_secret = ""
 access_key = ""
 access_secret = ""
 bearer_token = ""
+
+# Εδώ βάζουμε τους λογαριασμούς Twitter που θέλουμε να κατεβάσουμε
+# Μπορούμε να βάλουμε όσους θέλουμε
+# Ενδεικτικά έχω βάλει 3 λογαριασμούς Ελλήνων πολιτικών
+# Εσείς βάλτε τους δικούς σας
+
+accounts = ['AdonisGeorgiadi','PanosKammenos','rachelmakri']
