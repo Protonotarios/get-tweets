8428619b73b4b9f7371f23e61453f710a968337e
Ioannis Protonotarios <ioannis@protonotarios.eu>
2016-11-27 16:11:49 +0200

    Αλλαγή στηλών: κείμενο -> 2η στήλη
    Αλλαγή στηλών ώστε το κείμενο να είναι 2η στήλη

--- get-tweets.py
+++ get-tweets.py
@@ -58,14 +58,14 @@ def get_all_tweets(screen_name):
     # Επεξήγηση των πεδίων εδώ: https://dev.twitter.com/overview/api/tweets
     # Τα πεδία που δύναται να περιέχουν Ελληνικά πρέπει να κωδικοποιηθούν σε UTF-8
     # Μετατροπή των tweets σε διδιάστατο πίνακα ο οποίος μετά θα γραφτεί στο csv
-    outtweets = [[tweet.id_str, tweet.created_at, tweet.retweet_count, tweet.favorite_count, tweet.text.encode("utf-8")] 
+    outtweets = [[tweet.id_str, tweet.text.encode("utf-8"), tweet.created_at, tweet.retweet_count, tweet.favorite_count] 
                   for tweet in alltweets]
 	
     # Δημιουργία του εκάστοτε csv	
     with open('%s_tweets.csv' % screen_name, 'wb') as f:
         writer = csv.writer(f)
         # Δημιουργία κεφαλίδων στηλών
-        writer.writerow(["Αναγνωριστικό tweet","Ημερομηνία δημιουργίας","Αριθμός retweets","Αριθμός αγαπημένων","Κείμενο"])
+        writer.writerow(["Αναγνωριστικό tweet","Κείμενο","Ημερομηνία δημιουργίας","Αριθμός retweets","Αριθμός αγαπημένων"])
         # Εγγραφή των tweets
         writer.writerows(outtweets)
   
