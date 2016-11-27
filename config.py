182614b9ec8bd365350d8a99bcc102d3da3d2f6a
Ioannis Protonotarios <ioannis@protonotarios.eu>
2016-11-27 13:19:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# encoding: utf-8
+# encoding: utf-8 # Απαραίτητο όταν υπάρχουν Ελληνικά
 
 # ΑΡΧΕΙΟ ΡΥΘΜΙΣΕΩΝ
 
@@ -8,15 +8,18 @@
 # μέσω της Διαχείρισης Εφαρμογών του Twitter στη διεύθυνση:
 #	https://apps.twitter.com/app/new
 
-consumer_key = ""
-consumer_secret = ""
-access_key = ""
-access_secret = ""
-bearer_token = ""
+consumer_key = "29DCDI3ckG0HVOLk4KQumes2f"
+consumer_secret = "c1PfASwhv3hi04S7OQOQqJAe9Ac6cBIYAPlpIEqbjIwgan8MoO"
+access_key = "82060207-dEx2Er4h7dFQOL1uePQW4GJPsipba34FT870ctbDG"
+access_secret = "92EwdsXWrD5PXxHaqvqz50R4pAiO5fEj97dPugvasVlRB"
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAG44uQAAAAAAJ1tlA0%2B4xV5zSwIGSXvq0chgonw%3DG60P0Zdd6jEwxxUDN8sErA9J4ZfJ3C1Yc350EghdGFly0NLrPA"
 
 # Εδώ βάζουμε τους λογαριασμούς Twitter που θέλουμε να κατεβάσουμε
 # Μπορούμε να βάλουμε όσους θέλουμε
 # Ενδεικτικά έχω βάλει 3 λογαριασμούς Ελλήνων πολιτικών
 # Εσείς βάλτε τους δικούς σας
 
-accounts = ['AdonisGeorgiadi','PanosKammenos','rachelmakri']
+# Παράδειγμα:
+# accounts = ['AdonisGeorgiadi','PanosKammenos','rachelmakri']
+
+accounts = []
