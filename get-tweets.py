b997ce4fa69d84028a4a8be725aac7716882b91f
Ioannis Protonotarios <ioannis@protonotarios.eu>
2016-12-16 19:42:57 +0200

    Νέα δυνατότητα

--- get-tweets.py
+++ get-tweets.py
@@ -3,6 +3,7 @@
 
 import tweepy #https://github.com/tweepy/tweepy
 import csv
+from datetime import datetime, timedelta
 
 # Φόρτωση του αρχείου ρυθμίσεων config.py 
 # με τα διαπιστευτήρια για το API του Twitter 
@@ -10,8 +11,14 @@ import csv
 config = {}
 execfile("config.py", config)
 
+combined_out_tweets = []
+# Ορίζω αυθαίρετα τη μέγιστη και ελάχιστη ημερομηνία στη σημερινή
+min_date = max_date = datetime.now().date()
+#min_date = datetime.now().date()
 
 def get_all_tweets(screen_name):
+    global min_date 
+    
     # Το Twitter επιτρέπει μέσω του API τη λήψη μόνο των τελευταίων 3240 tweets ενός χρήστη
     
     # Ταυτοποίηση μέσω των διαπιστευτηρίων μας στο Twitter
@@ -70,6 +77,17 @@ def get_all_tweets(screen_name):
         writer.writerows(outtweets)
   
     print "Τα tweets εγγράφηκαν στο αρχείο %s_tweets.csv" % (screen_name)
+    
+    #partial_out_tweets = [[tweet.created_at.date(), tweet.retweet_count, tweet.favorite_count, screen_name] 
+    #              for tweet in alltweets]
+    partial_out_tweets = []
+    for tweet in alltweets:
+        tweet_date = tweet.created_at.date()
+        partial_out_tweets.append([tweet_date, tweet.retweet_count, tweet.favorite_count, screen_name])
+        if tweet_date < min_date:
+            min_date = tweet_date
+    
+    combined_out_tweets.extend(partial_out_tweets)
 	
     pass
 
@@ -81,3 +99,55 @@ if __name__ == '__main__':
         print 'Η λήψη tweets για το λογαριασμό %s ολοκληρώθηκε' % (account)
         print '-------------------------------'
     print 'Η λήψη tweets για όλους τους λογαριασμούς ολοκληρώθηκε'
+    
+    
+    # Δημιουργία του συνολικού csv	
+    with open('combined_tweets.csv', 'wb') as f:
+        writer = csv.writer(f)
+        # Δημιουργία κεφαλίδων στηλών
+        writer.writerow(['Ημερομηνία δημιουργίας','Αριθμός retweets','Αριθμός αγαπημένων','Χρήστης'])
+        # Εγγραφή των tweets
+        writer.writerows(combined_out_tweets)
+    print '-------------------------------'
+    print 'Τα συνδυασμένα δεδομένα εγγράφηκαν στο αρχείο combined_tweets.csv'
+    print 'Παλαιότερη ημερομηνία', min_date
+    date_delta = max_date - min_date
+    print date_delta
+    
+    final_tweets = []
+    test_date = min_date
+    while test_date <= max_date:
+        tweet_count = retweet_count = favorite_count = 0
+        current_line = [test_date]
+        for n, account in enumerate(config['accounts']):
+            current_line.extend([0,0,0])
+        for tweet in combined_out_tweets:
+            if tweet[0] == test_date:
+                for n, account in enumerate(config['accounts']):
+                    if config['accounts'][n] == tweet[3]:
+                        current_line[n * 3 + 1] += 1 # άθροιση tweets n-οστού χρήστη
+                        current_line[n * 3 + 2] += tweet[1]  # άθροιση retweets n-οστού χρήστη
+                        current_line[n * 3 + 3] += tweet[2] # άθροιση αγαπημένων n-οστού χρήστη
+                        #tweet_count += 1
+#                        retweet_count += tweet[1]
+#                        favorite_count += tweet[2]
+#                        user_tweets.append([tweet_count, retweet_count, favorite_count])
+#                tweet_count += 1
+#                retweet_count += tweet[1]
+#                favorite_count += tweet[2]
+#        final_tweets.append([test_date, tweet_count, retweet_count, favorite_count])
+        #print current_line
+        final_tweets.append(current_line)
+        test_date += timedelta(days=1)
+    #print final_tweets
+    
+    # Δημιουργία του τελικού csv	
+    with open('final_tweets.csv', 'wb') as f:
+        writer = csv.writer(f)
+        # Δημιουργία κεφαλίδων στηλών
+        header_line = ['Ημερομηνία']
+        for n, account in enumerate(config['accounts']):
+            header_line.extend(['Tweets '+account,'Retweets '+account,'Αγαπημένα '+account])
+        writer.writerow(header_line)
+        # Εγγραφή των tweets
+        writer.writerows(final_tweets)
