b9da2630a9013dcadbf2299a99807fdcce685d89
Ioannis Protonotarios <ioannis@protonotarios.eu>
2016-12-19 20:28:47 +0200

    Ενημέρωση

--- config.py
+++ config.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
-# encoding: utf-8
+# -*- coding: UTF-8 -*-
 
-consumer_key = ""
-consumer_secret = ""
-access_key = ""
-access_secret = ""
+# ΑΡΧΕΙΟ ΡΥΘΜΙΣΕΩΝ
 
+# Καταχωρίστε τα διαπιστευτήριά σας για το API του Twitter
+# Αυτά τα διαπιστευτήρια μπορείτε να τα δημιουργήσετε
+# μέσω της Διαχείρισης Εφαρμογών του Twitter στη διεύθυνση:
+#	https://apps.twitter.com/app/new
 
-# accounts = ['AdonisGeorgiadi','PanosKammenos','rachelmakri']
+# Τα ακόλουθα διαπιστευτήρια είναι ενδεικτικά και δεν λειτουργούν.
+# Αντικαταστήστε τα με τα δικά σας.
 
-accounts = []
+consumer_key = 'DlNW4SJRLYrO4CtCxhtjW1EN1'
+consumer_secret = 'mWngAcydMQMIkSFce2SLleg9yMiSLIGHedtAP6okYDepNUicWV'
+access_key = '82060207-dEx2Er4h7dFQOL1uePQW4GJPsipba34FT870ctbDG'
+access_secret = '92EwdsXWrD5PXxHaqvqz50R4pAiO5fEj97dPugvasVlRB'
+
+# Καταχωρίστε τα ονόματα χρήστη των λογαριασμών Twitter 
+# που θέλετε να συλλέξετε, σύμφωνα με το ακόλουθο υπόδειγμα.
+
+# Αντικαταστήστε τα με τα δικά σας.
+
+accounts = ['AdonisGeorgiadi','PanosKammenos','rachelmakri']
+
+# Καταχωρίστε το όνομα αρχείου του συναισθηματικού λεξικού
+# που θέλετε να χρησιμοποιήσετε.
+
+lexicon = 'lexico.txt'
